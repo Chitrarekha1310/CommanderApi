eca873c6b6c987c145513c9f4d62aad879b56783
Chitrarekha1310 <31435379+Chitrarekha1310@users.noreply.github.com>
2020-08-13 13:37:57 +0530

    COmmander APi

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Commander.Models;
+using Commander.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    //Route api/controller
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+            private readonly ICommanderRepo _repository;
+            //dependency injection
+            public CommandsController(ICommanderRepo repository)
+            {
+                _repository = repository;
+            }
+            [HttpGet]
+            public ActionResult <IEnumerable<Command>> GetAllCommands()
+            {
+                var commands = _repository.GetAppCommands();
+                return Ok(commands);
+            }
+
+            //defining route as api/command/id
+            [HttpGet("{id}")]
+            public ActionResult<Command> GetCommandById (int id)
+            {
+                var commandItem = _repository.GetCommandById(id);
+                return Ok(commandItem);
+            }
+    }
+}
